8a25943a2d145675df2ebfa1bf97630a393793c5
hughhu <hughhuxiaojun@outlook.com>
2020-01-14 10:56:44 +0800

    ;

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      //禁止百度等搜索引擎自动转码
+      { hid: 'Cache-Contro', content: 'no-transform', 'http-equiv': 'Cache-Control' },
+      { hid: 'Cache-Contro2', content: 'no-siteapp', 'http-equiv': 'Cache-Control' },
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
